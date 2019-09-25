a1d6d90f5c4d64ab59d5eca1d82b183028236afb
Dou Ya <xiao.466@osu.edu>
2019-09-25 01:45:55 -0400

    Enable switch people

--- mock_interview/interview.py
+++ mock_interview/interview.py
@@ -4,15 +4,28 @@ from speak import *
 import time
 
 filenames = ["faculty.png"]
+figure_path = "./pic/"
+figures = [
+        figure_path+"faculty.png",
+        figure_path+"Jessica_talk.png",
+        figure_path+"Alan_talk.png",
+        figure_path+"Lucas_talk.png",
+        figure_path+"John_talk.png",
+        figure_path+"Stefan_talk.png",
+        figure_path+"Robert_talk.png"
+        ]
+w_scale = 1.45
+h_scale = 1.4
 
 class TestFrame(wx.Frame):
     def __init__(self): 
-        wx.Frame.__init__(self, None, -1, 'My Python App',size=(1500, 760))
+        self.index = 0
+        wx.Frame.__init__(self, None, -1, 'My Python App',size=(1400, 760))
 
-        image = wx.Image('faculty.png', wx.BITMAP_TYPE_ANY)
+        image = wx.Image(figures[0], wx.BITMAP_TYPE_ANY)
         w = image.GetWidth()
         h = image.GetHeight()
-        image = image.Scale(w/1.4, h/1.4)#2 缩小图像
+        image = image.Scale(w/w_scale, h/h_scale) # Scale Image 
 
         panel = wx.Panel(self, -1)
         temp = image.ConvertToBitmap()
@@ -20,31 +33,47 @@ class TestFrame(wx.Frame):
         self.bmp = wx.StaticBitmap(panel,-1,temp,pos=(0, 0),size=size)
 
         self.button = wx.Button(panel, -1, "1", pos=(50, 700))
-        self.Bind(wx.EVT_BUTTON, self.OnClickFemale, self.button)
+        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button)
         self.button.SetDefault()
 
-        self.button2 = wx.Button(panel, -1, "1", pos=(100, 700))
-        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button2)
-        self.button3 = wx.Button(panel, -1, "1", pos=(300, 700))
-        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button3)
-        self.button4 = wx.Button(panel, -1, "1", pos=(500, 700))
-        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button4)
-        self.button5 = wx.Button(panel, -1, "1", pos=(700, 700))
-        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button5)
-        self.button6 = wx.Button(panel, -1, "1", pos=(900, 700))
-        self.Bind(wx.EVT_BUTTON, self.OnClick, self.button6)
+        panel.Bind(wx.EVT_KEY_DOWN, self.OnKeyDown)
+
+    def getVoice(self):
+        # 0 for male voice, 1 for female voice
+        return 1 if self.index == 1 else 0
+
+    def changePerson(self, kc):
+        self.index = int(chr(kc))
+        image = wx.Image(figures[self.index], wx.BITMAP_TYPE_ANY)
+        w = image.GetWidth()
+        h = image.GetHeight()
+        image = image.Scale(w/w_scale, h/h_scale) # Scale Image 
+        self.bmp.SetBitmap(wx.BitmapFromImage(image))
+        self.Show()
+
+    def OnKeyDown(self, event):
+        kc=event.GetKeyCode()
+        if ord('0') <= kc <= ord('6'):
+            self.changePerson(kc)
+        elif ord('t') == kc or ord('T') == kc:
+            self.askTeaching()
+        elif ord('i') == kc or ord('I') == kc:
+            self.introduction()
+
+    def introduction(self):
+        content = "Hello ChongNon, thank you for applying to our program!"
+        self.speak(content)
+
+    def askTeaching(self):
+        content = "Hello, ChongNon! Thank you for applying to our program. How to you spell your name?"
+        subprocess.call(["python", "speak.py", content, str(self.getVoice())])
 
     def OnClick(self, event):
         content = "How to you spell your name?"
-        try:
-            subprocess.call(["python", "speak.py"])
-        except:
-            self.button2.SetLabel("Error")
+        subprocess.call(["python", "speak.py"])
 
-        self.button3.SetLabel("Clicked")
-
-    def OnClickFemale(self, event):
-        self.button.SetLabel("Clicked")
+    def speak(self, content):
+        subprocess.call(["python", "speak.py", content, str(self.getVoice())])
 
 if __name__ == '__main__':
 
--- mock_interview/speak.py
+++ mock_interview/speak.py
@@ -1,20 +1,32 @@
 import pyttsx3
 import random
+import sys
 
 
-def speak(content):
+def speak(content, isMale):
 
 # content = "How would you get undergraduates excited about the field of X?"
     engine = pyttsx3.init()
+    engine.setProperty('rate', 220)
     voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[10].id)
     engine.setProperty('voice', voices[7].id)
+    engine.setProperty('voice', voices[0].id)
     engine.setProperty('voice', voices[17].id)
     engine.setProperty('voice', voices[26].id)
-    engine.setProperty('voice', voices[0].id)
-    engine.setProperty('voice', voices[10].id)
-    engine.setProperty('voice', voices[29].id)
+
+    voice_choice = 0 if isMale== 0 else 17
+    engine.setProperty('voice', voices[voice_choice].id)
     engine.say(content)
     # pyttsx3.speak("I will speak this text")
     # engine.say('The quick brown fox jumped over the lazy dog.')
     engine.runAndWait()
-    engine.stop()
+
+if __name__ == "__main__":
+    content = "Can you talk a little bit about your current research?"
+    isMale = 0
+    if len(sys.argv) >= 2:
+        content = sys.argv[1]
+    if len(sys.argv) >= 3:
+        isFemale = int(sys.argv[2])
+    speak(content, isFemale)
